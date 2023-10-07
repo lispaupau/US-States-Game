9d3a1ec97acac9c70695568465e5feb4beba9d29
pndao <pndao@inbox.ru>
2023-10-07 20:36:20 +0300

    update

--- main.py
+++ main.py
@@ -18,10 +18,7 @@ while True:
     answer_state = screen.textinput(title=f'{len(correct_answer)}/50 States Correct',
                                     prompt='What`s another states name?').title()
     if answer_state == 'Exit':
-        missing_states = []
-        for state in all_states:
-            if state not in correct_answer:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in correct_answer]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv('states_to_learn.csv')
         break
